dd865365584abbc479d6c8fe01cfa4f95ebba212
Vitor-App <vitorapolinariob1@gmail.com>
2024-03-20 11:34:42 -0300

    password generation values were set for the id "password" using const in the .JS file.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789"
+const symbol = "&@^!%@*<>_/=[()*"
+
